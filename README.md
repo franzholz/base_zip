d3d9f4b831c543b4826f4eaa787acc18a4237d68
Franz Holzinger <franz.holzinger@festwein.de>
2022-07-23 22:39:22 +0200

    inital version with ZipStream-PHP 2.2.0

---
+++ README.md
@@ -0,0 +1,6 @@
+base_zip
+========
+This provides the ZipStream-PHP library.
+It is required for older versions of base_excel, where external libraries are not included into the PHAR file.
+
+
---
+++ ext_emconf.php
@@ -0,0 +1,30 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "base_zip".
+########################################################################
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'ZIP Library',
+    'description' => 'A library to download ZIP files. ZipStream-PHP 2.2.0',
+    'category' => 'misc',
+    'version' => '0.0.1',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearcacheonload' => 1,
+    'author' => 'Jonatan Männchen, Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'author_company' => 'jambage.com',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.4.0-8.1.99',
+            'typo3' => '9.5.0-11.5.99',
+            'base_enum' => '0.0.2-0.0.0'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
